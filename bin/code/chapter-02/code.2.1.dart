a1c9a4b0d91085ba1b89206085148a261a6adc19
William Dias <williamdiasvargas@gmail.com>
2020-03-14 14:52:42 -0300

    chapter 2

---
+++ bin/code/chapter-02/code.2.1.dart
@@ -0,0 +1,16 @@
+// code 2.1
+main(List<String> arguments) {
+    bool firstButtonTouch = true;
+      bool secondButtonTouch = false;
+        bool thirdButtonTouch = true;
+          bool fourthButtonTouch = false;
+            if(firstButtonTouch) print("The giant starts running.");
+               else print("To stop the giant please touch the second button.");
+                 if(secondButtonTouch) print("The giant stops.");
+                   else print("You have not touched the second button.");
+                     print("Touch any button to start the game.");
+                       if(thirdButtonTouch) print("The giant goes to sleep.");
+                         else print("You have not touched any button.");
+                           if(fourthButtonTouch) print("The giant wakes up.");
+                             else print("You have not touched any button.");
+                             }
---
+++ bin/code/chapter-02/code.2.2.dart
@@ -0,0 +1,9 @@
+main(List<String> args) {
+  bool firstButtonTouch = true;
+  var firstButtonUntouch;
+  bool secondButtonTouch = true;
+  bool thirdButtonTouch = true;
+  bool fourthButtonTouch = false;
+  firstButtonUntouch ??= firstButtonTouch;
+  firstButtonTouch = false;
+}
